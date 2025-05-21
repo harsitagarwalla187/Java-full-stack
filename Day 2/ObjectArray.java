ecf24ba0eb66db352e5a17adbfa8b96c5efe7215
harsit <harsitagarwalla187@gmail.com>
2025-05-21 10:01:15 +0530

    Day 2 HomeWork

--- Day 2/ObjectArray.java
+++ Day 2/ObjectArray.java
@@ -2,10 +2,29 @@ class Student {
      int rollNo;
      String name;
 
+     Student(int rollNo, String name) {
+          this.rollNo = rollNo;
+          this.name = name;
+     }
+
+     public void print() {
+          System.out.println("Student Roll No: " + rollNo);
+          System.out.println("Student Name: " + name);
+     }
+ 
 }
 
 public class ObjectArray {
      public static void main(String[] args) {
           Student std[] = new Student[100];
+
+          for(int i=0; i<100; i++) {
+               String name = "Student" + i;
+               std[i] = new Student(i, name);
+          }
+
+          for(int i=0; i<100; i++) {
+               std[i].print();
+          }
      }
 }
