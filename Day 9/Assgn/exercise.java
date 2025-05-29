23d7f6f1ffbfa1a36e8db14556d250101965897a
harsit <harsitagarwalla187@gmail.com>
2025-05-29 11:16:34 +0530

    Update Day 9  assignment

--- Day 9/Assgn/exercise.java
+++ Day 9/Assgn/exercise.java
@@ -1,4 +1,7 @@
 // 1. Arithmetic Exception
 
-// 2. Error: trying to excess private member
+// 2. 
+//   Inner class cannot extends outer class
+//   Trying to excess private member
+//   Incorrect Insatantiation of Inner class
 
