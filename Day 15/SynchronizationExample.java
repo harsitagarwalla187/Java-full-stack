e970df74c89c761ae55b3b6bc4779a53245ec673
harsit <harsitagarwalla187@gmail.com>
2025-06-06 09:29:57 +0530

    Day 15 works on Concurrency

--- Day 15/SynchronizationExample.java
+++ Day 15/SynchronizationExample.java
@@ -4,10 +4,33 @@ class Account {
     Account() {
         balance = 10_000;
     }
+
+    public synchronized void withdraw() {
+        balance -= 1000;
+        System.out.println(balance);
+    }
+}
+
+class User extends Thread {
+    Account account;
+
+    User(Account account) {
+        this.account = account;
+    }
+
+    public void run() {
+        account.withdraw();
+    }
 }
 
 public class SynchronizationExample {
     public static void main(String[] args) {
 
+        Account ac = new Account(); 
+        User u1 = new User(ac);
+        User u2 = new User(ac);
+
+        u1.start();
+        u2.start();
     }
 }
